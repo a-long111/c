a8b81e3f9cdea536a79c2778d943afc993a970bc
ASCII-58 <119689982+xyxy1029@users.noreply.github.com>
2024-12-29 12:11:43 +0800

    modified:   main.c

--- main.c
+++ main.c
@@ -16,10 +16,41 @@ struct Employee{
 
 int main(){
     for(;;){
-        printf("**************菜单选项**************\n1. Add Employee\n************************************");
+        printf("**************菜单选项**************\n");
+        printf("1.添加员工信息\n");
+        printf("2.删除员工信息\n");
+        printf("3.修改员工信息\n");
+        printf("4. 查询员工信息\n");
+        printf("5. 删除员工信息\n");
+        printf("6. 保存记录至文件\n");
+        printf("7. 从文件读取记录\n");
+        printf("8. 退出\n");
+        printf("************************************\n");
         //提示输入
         int choice;
-        printf("请选择:");
+        printf("\n请选择:");
         scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            //添加员工信息
+        case 2:
+            //删除员工信息
+        case 3:
+            //修改员工信息
+        case 4:
+            // 查询员工信息
+        case 5:
+            // 删除员工信息
+        case 6:
+            // 保存记录至文件
+        case 7:
+            // 从文件读取记录
+        case 8:
+            // 退出
+            return 0;
+        default:
+            printf("输入错误,请重新输入!\n");
+        }
     }
 }
